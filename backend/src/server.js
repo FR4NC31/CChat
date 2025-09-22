34808ee721f522d329219de751f861b41ec7b2d2
FR4NC31 <francisedgard16@gmail.com>
2025-09-22 23:07:24 +0800

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 //routes
 import authRoutes from './routes/auth.route.js'
@@ -8,10 +9,19 @@ import messageRoutes from './routes/message.route.js'
 dotenv.config()
 
 const app = express()
+const __dirname = path.resolve()
 
 const PORT = process.env.PORT || 3000
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+//make ready fot development
+if (process.env.NODE_env === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend','dist','index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log('Server started on port:', + PORT))
